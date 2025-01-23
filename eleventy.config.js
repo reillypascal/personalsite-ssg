57f97c4c97aef80fe42fbcd92a3af8682d8c5278
Reilly Spitzfaden <reillypascal@gmail.com>
2025-01-22 23:41:57 -0500

    added notes page

--- eleventy.config.js
+++ eleventy.config.js
@@ -36,6 +36,12 @@ module.exports = async function (eleventyConfig) {
     </div>`;
   });
 
+  eleventyConfig.addShortcode("notefooter", (title, url) => {
+    return `<div class="email-reply">
+        <a href="mailto:reillypascal@gmail.com?subject=Re: ${title}">Reply via email</a>
+    </div>`;
+  });
+
   eleventyConfig.addShortcode("likedfooter", (title, url) => {
     return `<div class="dinkus"><p>***</p></div>
     <div class="email-reply">
