a27fe70cd6302f8bafc0a0cece0cb266ac585e10
Reilly Spitzfaden <reillypascal@gmail.com>
2025-01-26 16:20:58 -0500

    working on getting webmentions from mastodon

--- pages/_data/webmentions.js
+++ pages/_data/webmentions.js
@@ -4,12 +4,18 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 module.exports = async function () {
 	const WEBMENTIONS_RS = process.env.WEBMENTION_IO_TOKEN;
 	const url = `https://webmention.io/api/mentions.jf2?token=${WEBMENTIONS_RS}&per-page=1000`;
-	const res = EleventyFetch(url, {
-		duration: "1h",
-		type: "json",
-	});
-	const webmentions = await res;
-	return {
-		mentions: webmentions.children,
-	};
+    try {
+        const webmentions = await EleventyFetch(url, {
+            duration: "1h",
+            type: "json",
+        });
+        return {
+            mentions: webmentions.children
+        };
+    } catch (e) {
+        console.log("Failed getting webmentions");
+        return {
+            mentions: "none"
+        };
+    }
 };
