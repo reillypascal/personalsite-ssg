1af1c06b832d726b5b4bac93caa06244b38b9ff6
Reilly Spitzfaden <reillypascal@gmail.com>
2025-01-27 20:12:54 -0500

    using webmention plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ let markdownIt = require("markdown-it");
 let markdownItFootnote = require("markdown-it-footnote");
 const { DateTime } = require("luxon");
 const sanitizeHTML = require("sanitize-html");
+const Webmentions = require("eleventy-plugin-webmentions");
 
 module.exports = async function (eleventyConfig) {
   // passthrough copies
@@ -149,6 +150,11 @@ module.exports = async function (eleventyConfig) {
   //     },
   //   },
   // });
+
+  eleventyConfig.addPlugin(Webmentions, {
+    domain: "reillyspitzfaden.com",
+    token: "-PsKFuieg-7U9kQK5X8cqg",
+  });
 
   // markdown-it
   let options = {
