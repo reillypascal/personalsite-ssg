f76a1dfd7f4c9960fd3158f798f4149c8191f2fe
Reilly Spitzfaden <reillypascal@gmail.com>
2025-02-05 22:32:02 -0500

    js to mjs for module

--- pages/_data/webmentions.mjs
+++ pages/_data/webmentions.mjs
@@ -5,7 +5,7 @@ export default async function () {
 	const url = `https://webmention.io/api/mentions.jf2?token=${process.env.WEBMENTION_IO_TOKEN}&per-page=1000`;
 	try {
 		const webmentions = await EleventyFetch(url, {
-			duration: "0s",
+			duration: "1h",
 			type: "json",
 		});
 		return {
