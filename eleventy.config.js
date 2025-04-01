d66f84a3b640cd5a5e63b413b91cb9d37da055f4
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-01 01:48:39 -0400

    simplified sanitizer tags w/ 'concat'

--- eleventy.config.js
+++ eleventy.config.js
@@ -22,16 +22,8 @@ module.exports = async function (eleventyConfig) {
   // filters
   eleventyConfig.addFilter("sanitizeHTML", (html) => {
     return sanitizeHTML(html, {
-      // full available list — I just want to remove link, script, style, etc.
-      allowedTags: [
-        "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
-        "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
-        "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
-        "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
-        "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
-        "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
-        "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "img", "audio"
-      ],
+      // adds on to full available list
+      allowedTags: sanitizeHTML.defaults.allowedTags.concat([ 'audio', 'img' ]),
       allowedAttributes: {
         a: [ 'href', 'name', 'target' ],
         audio: [ 'controls', 'src' ],
