bcc40def041d6c466e188318b1650b11adf68db8
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-11 18:57:54 -0400

    added webmentions filter python script

---
+++ webmentions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json, os, subprocess, urllib.request
+from dotenv import load_dotenv
+
+load_dotenv()
+
+wm_token = os.getenv('WEBMENTION_IO_TOKEN')
+
+with urllib.request.urlopen(f"https://webmention.io/api/mentions.jf2?token={wm_token}&per-page=1000") as response:
+    res = response.read()
+
+json_dict = json.loads(res.decode("utf-8"))
+
+webmentions = { "mentions" : [] }
+for entry in json_dict.get("children"):
+    if "https://brid.gy/" not in entry.get("wm-source"):
+        webmentions.get("mentions").append(entry)
+
+output_filename = 'webmentions.json'
+
+with open(output_filename, 'w', encoding='utf-8') as output_file:
+    json.dump(webmentions, output_file, ensure_ascii=False, indent=4)
+
+# open_file = subprocess.run(f"codium {output_filename}", check=True, text=True)
+# https://stackoverflow.com/a/51950538
