64cb50bfe35192d64fcae645642db9a0c6a22487
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-25 01:46:14 -0400

    more button cleanup conditions

--- styles/menus.js
+++ styles/menus.js
@@ -1,12 +1,19 @@
 let buttonStates = {};
 
 function dropdownCleanup(event) {
+    let buttons = document.getElementsByClassName("dropbtn");
     // event represented by `this` keyword
-    if (buttonStates[this.id] == 1) {
-        buttonStates[this.id] = 0;
-        this.blur();
+    if (buttonStates[event.target.id] == 1) {
+        buttonStates[event.target.id] = 0;
+        event.target.blur();
     } else {
-        buttonStates[this.id] = 1;
+        buttonStates[event.target.id] = 1;
+        for (const element of buttons) {
+            // if (!element.classList.contains("navIcon") && element.id != this.id) {
+            if (element.id != event.target.id) {
+                buttonStates[element.id] = 0;
+            }
+        }
     }
 }
 
@@ -15,5 +22,14 @@ window.addEventListener("load", (event) => {
     let buttons = document.getElementsByClassName("dropbtn");
     for (const element of buttons) {
         element.addEventListener("click", dropdownCleanup);
+    }
+});
+
+window.addEventListener("click", (event) => {
+    if (!event.target.classList.contains("dropbtn")) {
+        for (const [key, value] of Object.entries(buttonStates)) {
+            buttonStates[key] = 0;
+        }
+        // console.log(buttonStates);
     }
 });
